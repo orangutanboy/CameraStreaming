709937703f2c4785af6187d945dae6076ae23898
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-07-03 20:24:08 +0100

    Tidied up for public consumption

--- Program.cs
+++ Program.cs
@@ -1,54 +1,73 @@
 ﻿using System;
 using Gadgeteer.Modules.GHIElectronics;
 using GHI.Premium.System;
-using Microsoft.SPOT;
 using GTM = Gadgeteer.Modules;
 
 namespace CameraStreaming
 {
     public partial class Program
     {
-        Bitmap _bitmap = new Bitmap(320, 240);
-
-        private Bluetooth.Client client;
+        private Bluetooth.Client _bluetoothClient;
         private Gadgeteer.Timer _connectTimer;
 
         void ProgramStarted()
         {
-            camera.PictureCaptured += new Camera.PictureCapturedEventHandler(camera_PictureCaptured);
-
-            button.ButtonPressed += new GTM.GHIElectronics.Button.ButtonEventHandler(button_ButtonPressed);
+            camera.PictureCaptured += (s, picture) => StreamPictureToBluetooth(picture);
+            button.ButtonPressed += (s, b) => camera.TakePicture();
 
             bluetooth.SetDeviceName("Gadgeteer");
             bluetooth.SetPinCode("0000");
 
-            client = bluetooth.ClientMode;
+            _bluetoothClient = bluetooth.ClientMode;
 
-            _connectTimer = new Gadgeteer.Timer(3000, Gadgeteer.Timer.BehaviorType.RunContinuously);
-            _connectTimer.Tick += new Gadgeteer.Timer.TickEventHandler(timer_Tick);
+            //Initialise bluetooth (give it 3 seconds to boot up, SOP)
+            _connectTimer = new Gadgeteer.Timer(3000, Gadgeteer.Timer.BehaviorType.RunOnce);
+            _connectTimer.Tick += t => _bluetoothClient.EnterPairingMode();
             _connectTimer.Start();
         }
 
-        void camera_PictureCaptured(Camera sender, Gadgeteer.Picture picture)
+        //void camera_PictureCaptured(Camera sender, Gadgeteer.Picture picture)
+        void StreamPictureToBluetooth(Gadgeteer.Picture picture)
         {
+            //Convert the Picture to a bitmap
             var bitmap = picture.MakeBitmap();
+
+            //Display the picture on the LCD
             bitmap.Flush();
 
-            var width = 320;
-            var height = 240;
+            //Get the bytes for the windows-compatible BMP format
+            var bmpBuffer = GetBMPBytes(bitmap);
 
-            var bmpBuffer = new byte[width * height * 3 + 54];
+            ChunkStreamToBluetooth(bmpBuffer);
+        }
 
-            Util.BitmapToBMPFile(bitmap.GetBitmap(), width, height, bmpBuffer);            
-
+        private void ChunkStreamToBluetooth(byte[] bmpBuffer)
+        {
+            // 2478 * 93 === 320 * 240 * 3 + 54 === 230454
             var chunkSize = 2478;
+
+            //Create a byte array to hold the chunks prior to streaming
             var sendArray = new byte[chunkSize];
+
             for (var i = 0; i < 93; ++i)
             {
+                //Copy the nect chunk
                 Array.Copy(bmpBuffer, i * chunkSize, sendArray, 0, chunkSize);
+
+                //Write to the bluetooth stream
                 bluetooth.serialPort.Write(sendArray);
                 bluetooth.serialPort.Flush();
             }
+        }
+
+        private static byte[] GetBMPBytes(Microsoft.SPOT.Bitmap bitmap)
+        {
+            //Convert the bitmap to a windows-compatible BMP
+            var width = bitmap.Width;
+            var height = bitmap.Height;
+            var bmpBuffer = new byte[width * height * 3 + 54];
+            Util.BitmapToBMPFile(bitmap.GetBitmap(), width, height, bmpBuffer);
+            return bmpBuffer;
         }
 
         void button_ButtonPressed(GTM.GHIElectronics.Button sender, GTM.GHIElectronics.Button.ButtonState state)
@@ -59,7 +78,7 @@ namespace CameraStreaming
         void timer_Tick(Gadgeteer.Timer timer)
         {
             _connectTimer.Stop();
-            client.EnterPairingMode();
+            _bluetoothClient.EnterPairingMode();
         }
     }
 }
